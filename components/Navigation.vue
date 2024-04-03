5af6f2d79d098a27cfc7b58c6183d50c9eaee8b0
BacaniLeon <leonbaani@gmail.com>
2024-04-03 02:41:19 +0200

    deploy

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,10 +3,10 @@
     <img src="../public/resources/images/logo.svg">
     <img src="../public/resources/images/menu.png" class="navBar__Menu" @click="showMenu">
     <div class="navBar__Div">
-    <NuxtLink to="/">HOME</NuxtLink>
-    <NuxtLink to="/about">ABOUT ME</NuxtLink>
-    <NuxtLink to="/my-experience">EXPERIENCE</NuxtLink>
-    <NuxtLink to="/contact" class="navBar__Button">CONTACT</NuxtLink>
+    <NuxtLink to="/" @click="changeOverflow()">HOME</NuxtLink>
+    <NuxtLink to="/about" @click="changeOverflow()" >ABOUT ME</NuxtLink>
+    <NuxtLink to="/my-experience" @click="changeOverflow()">EXPERIENCE</NuxtLink>
+    <NuxtLink to="/contact" class="navBar__Button" @click="changeOverflow()">CONTACT</NuxtLink>
   </div>
     
   </nav>
@@ -15,9 +15,23 @@
 <script setup>
 
 const menuHidden = ref(false);
-
 const showMenu = computed(() => {
  menuHidden.value=!menuHidden.value;
+ if(menuHidden.value===true){
+  var htmlElement = document.querySelector("html");
+  htmlElement.style.overflow="hidden";
+ }
+ if(menuHidden.value===false){
+  var htmlElement = document.querySelector("html");
+  htmlElement.style.overflow="visible";
+ }
 })
+
+function changeOverflow(){
+  var htmlElement = document.querySelector("html");
+  htmlElement.style.overflow='visible';
+}
+  
+
   
 </script>
